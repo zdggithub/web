c2a1f275859e91a5bb245a25a426e1cdbec99e64
zzgithub <zzgithub@163.com>
2015-01-03 23:46:57 +0800

    dddddd

--- src/main/java/com/jrj/payment/controller/BorrowController.java
+++ src/main/java/com/jrj/payment/controller/BorrowController.java
@@ -53,6 +53,7 @@ public class BorrowController {
     public @ResponseBody String insertBorrow1(Borrow bw,HttpServletRequest res) throws Exception {
     	System.out.println("ddddd");
     	System.out.println("ddddd");
+    	System.out.println("ddddd");
 //    	System.out.println(bw.getTitle());
     	bw.setUserId("276347623");
     	res.setAttribute("borrow", BorrowService.insertBorrow(bw));
